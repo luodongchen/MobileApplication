0585b4bf0c75a0c1466fe0d4ac41f59b6ee65c54
Dc.L <luodongchen@gmail.com>
2024-12-08 21:47:52 +0100

    assignment 08.12

--- app/src/main/java/com/example/homework/FragmentB.kt
+++ app/src/main/java/com/example/homework/FragmentB.kt
@@ -1,4 +1,5 @@
 package com.example.homework
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -13,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [Fragment_b.newInstance] factory method to
+ * Use the [FragmentB.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Fragment_b : Fragment() {
+class FragmentB : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -33,13 +34,15 @@ class Fragment_b : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_b, container, false)
 
-        // Example: Button to go back to Fragment_a
+        // 获取按钮并设置点击监听器
         val button: Button = view.findViewById(R.id.buttonB)
         button.setOnClickListener {
-            parentFragmentManager.popBackStack() // Navigate back to Fragment_a
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, LoginFragment.newInstance("param1", "param2"))
+                .addToBackStack(null)
+                .commit()
         }
 
         return view
@@ -52,11 +55,12 @@ class Fragment_b : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment Fragment_b.
+         * @return A new instance of fragment FragmentB.
          */
+        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            Fragment_b().apply {
+            FragmentB().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
